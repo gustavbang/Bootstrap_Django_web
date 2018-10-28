7d7e930522e1a41de3f2883714ebe371a6d9ead4
Gustav Bang <gustavbang96@gmail.com>
2018-10-28 14:26:58 +0100

    Added forms and url for adding data

--- bungy/urls.py
+++ bungy/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('info/', views.info, name='info'),
     path('overview', views.overview, name='overview'),
-    path('test', views.test, name='test')
+    path('test', views.test, name='test'),
+    path('login', views.login, name='login'),
+    path('loggedin', views.loggedin, name='loggedin')
 ]
--- bungy/views.py
+++ bungy/views.py
@@ -1,6 +1,7 @@
 import datetime
 
-from django.shortcuts import render
+#for forms.py
+from bungy.forms import LoginForm
 
 # Create your views here.
 
@@ -28,3 +29,20 @@ def test(request):
     today = datetime.datetime.now().date()
     daysOfWeek = ["mon", "tue", "wed", "thur", "fri", "sat", "sun"]
     return render(request, "test.html", {"today" : today, "days_of_week": daysOfWeek})
+
+
+def login(request):
+    return render(request, "login.html", {})
+
+
+def loggedin(request):
+    username = "not logged in"
+    if request.method == "POST":
+        MyLoginForm = LoginForm(request.POST)
+        if MyLoginForm.is_valid():
+            print("hej")
+            username = MyLoginForm.cleaned_data['username']
+        else:
+            MyLoginForm = LoginForm()
+
+    return render(request, "loggedin.html", {"username": username})
